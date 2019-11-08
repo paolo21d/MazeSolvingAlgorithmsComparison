1fdc6da2bd68758a887b4e532bccd653c337cf40
paolo21d <paolo21d@gmail.com>
2019-11-08 01:03:15 +0100

    Changed maze structure & begin implementation of BFS

--- src/main/java/data/Maze.java
+++ src/main/java/data/Maze.java
@@ -4,13 +4,17 @@ package data;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 @Getter
 @Setter
-public class Maze implements MazeCreator, MazeSolver, MazePrinter{
-    private List<List<Node>> mazeStructure;
+public class Maze implements MazeCreator, MazeSolver, MazePrinter {
+    private List<Node> mazeStructure;
+    private Node beginOfMaze;
+    private Node endOfMaze;
     private Integer sizeX, sizeY;
 
     public void readMazeStructureFromFile() {
@@ -26,6 +30,22 @@ public class Maze implements MazeCreator, MazeSolver, MazePrinter{
     }
 
     public List<Node> BFS() {
+        List<Boolean> visited = prepareVisitedList();
+        Queue<Node> nodeQueue = new LinkedList<>();
+        nodeQueue.add(beginOfMaze);
+        while (!nodeQueue.isEmpty()) {
+            Node node = nodeQueue.poll();
+            setVisitedFromCoordinates(visited, node.getX(), node.getY(), true);
+            if (checkMazeEnd(node))
+                break;
+            for (Node neighbour : node.getNeighbours()) {
+                if (isVisitedFromCoordinates(visited, neighbour.getX(), neighbour.getY()))
+                    continue;
+                nodeQueue.add(neighbour);
+            }
+        }
+
+        //TODO Implement backtracking (for every node I have to know from which node I came)
         return null;
     }
 
@@ -37,14 +57,31 @@ public class Maze implements MazeCreator, MazeSolver, MazePrinter{
         return null;
     }
 
-
-
-
-
-
+    /**
+     * @param x - X coordinate of Node
+     * @param y - Y coordinate of Node
+     * @return Node which have got coordinates (x,y)
+     */
     private Node getNode(Integer x, Integer y) {
-        return mazeStructure.get(x).get(y);
+        return mazeStructure.get(y * sizeX + x);
     }
 
+    private List<Boolean> prepareVisitedList() {
+        List<Boolean> visited = Arrays.asList(new Boolean[getSizeX() * getSizeY()]);
+        visited.forEach(v -> v = false);
+        return visited;
+    }
 
+    private Boolean isVisitedFromCoordinates(List<Boolean> visited, Integer x, Integer y) {
+        return visited.get(y * sizeX + x);
+    }
+
+    private void setVisitedFromCoordinates(List<Boolean> visited, Integer x, Integer y, Boolean value) {
+        Boolean v = visited.get(y * sizeX + x);
+        v = value;
+    }
+
+    private Boolean checkMazeEnd(Node node) {
+        return endOfMaze == node;
+    }
 }
--- src/main/java/data/Node.java
+++ src/main/java/data/Node.java
@@ -3,8 +3,12 @@ package data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Node {
-
+    private Integer x;
+    private Integer y;
+    private List<Node> neighbours;
 }
