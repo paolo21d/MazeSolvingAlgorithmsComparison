89140176024ae88e450a47ebea0c0505f6bdaf09
Quazan <alexer98@gmail.com>
2019-11-11 19:50:11 +0100

    Added javaDoc to getSimplifiedMazeSolution method

--- src/main/java/data/MazePrinter.java
+++ src/main/java/data/MazePrinter.java
@@ -7,4 +7,14 @@ public interface MazePrinter {
      * @return String contains x - maze columns, y - maze rows and structure which represents whole structure of maze
      */
     String getSimplifiedMazeStructure();
+
+
+    /**
+     * This method returns simplified view of maze with solution path
+     * x - path
+     * B - start of maze
+     * E - end of maze
+     * @return String contains x - maze columns, y - maze rows and structure which represents structure of maze
+     */
+    String getSimplifiedMazeSolution();
 }
